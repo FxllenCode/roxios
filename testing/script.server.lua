b97ae9b1ce343930644c9090a5dfbcc4f7c0a610
FxllenCode <eswebber07@gmail.com>
2022-04-24 14:58:13 -0400

    feat: everything lo

---
+++ testing/script.server.lua
@@ -0,0 +1,16 @@
+local BetterHttp = require(game.ReplicatedStorage.roxios)
+local HttpService = game:GetService("HttpService")
+BetterHttp.Request({
+	Url = "http://httpbin.org/post", -- This website helps debug HTTP requests
+	Method = "POST",
+	Headers = {
+		["Content-Type"] = "application/json", -- When sending JSON, set this!
+	},
+	Body = HttpService:JSONEncode({ hello = "world" }),
+})
+	:andThen(function(parsedResponse)
+		print(parsedResponse.data)
+	end)
+	:catch(function(error)
+		warn(error)
+	end)
